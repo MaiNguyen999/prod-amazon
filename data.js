0df187a98ae23c9bbad756590be67a3bff016f43
MaiNguyen999 <maikhanhca2019@gmail.com>
2021-01-26 16:19:08 -0600

    first commit

---
+++ data.js
@@ -0,0 +1,37 @@
+module.exports.data =  {
+    products: [
+        {
+            id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 45,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 4
+        },
+        {
+            id: '2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 55,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 3
+        },
+        {
+            id: '3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 4
+        },
+    ]
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var {data} = require('./data');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get("/api/products", (req, res)=>{
+    res.send(data.products);
+})
+
+app.get("/api/products/:id", (req, res)=>{
+    const productId = req.params.id;
+    if(productId)
+        res.send(data.products.find(x=>x.id===productId));
+    else
+        res.status(404).send({msg: "Product not found"});
+})
+
+app.listen(5000, () => {console.log("Server started at http://localhost:5000")})
